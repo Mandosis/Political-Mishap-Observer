22c792d253e73f24c65f7218e71b3b60a2b7c2ff
Chris Rabuse <rab233@live.com>
2016-04-07 16:36:26 -0500

    Server created with end points

---
+++ app.js
@@ -0,0 +1,34 @@
+// Dependencies
+var express = require('express');
+
+// Global Variables
+var republicans = ['British Dude', 'Monkey', 'Banjo Master', 'Turkey Guy'];
+var democrats = ['Overtly eating a bananna sandwhich  Person', 'Money Bags', 'Lives on Couch', 'Rocket Man'];
+
+// Middleware
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/republicans', function(req, res) {
+  res.send(republicans);
+});
+
+app.get('/democrats', function(req, res) {
+  res.send(democrats);
+});
+
+app.get('/winner', function(req, res) {
+  var winner = function() {
+    var candidates = republicans.concat(democrats);
+    var random = Math.floor(Math.random() * (1 + candidates.length - 1) + 1) - 1;
+
+    return candidates[random];
+  };
+  res.send(winner());
+});
+
+var server = app.listen(process.env.PORT || 3000, function() {
+  var port = server.address().port;
+  console.log('Server listening on port', port);
+})
